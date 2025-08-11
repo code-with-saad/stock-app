3320291f39bcbfdd4278e8bcd86424804a977c6f
Saad Kashif <115474924+code-with-saad@users.noreply.github.com>
2025-08-11 23:56:20 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('stock-app-v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './script.js',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
